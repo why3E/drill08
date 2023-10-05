cf4d1e2a8b1cf05d625582b4a79edccd60ab1a5d
wonju <jwwon00@naver.com>
2023-10-05 10:48:19 +0900

    소년 클래스 생성 및 11명 생성하는 팀 리스트 생성

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,16 +1,27 @@
 from pico2d import *
 
-
 # Game object class here
 class Grass:
     def __init__(self):
         self.image = load_image('grass.png')
 
     def draw(self):
-        self.image.draw(400, 300)
+        self.image.draw(400, 30)
 
     def update(self): pass
 
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0,90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame +1) %8
+        self.x +=5
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
 
 def handle_events():
     global running
@@ -24,15 +35,26 @@ def handle_events():
 
 def reset_world():
     global running
+    global grass
+    global team
+
     running = True
+    grass = Grass()
+    team = [Boy() for i in range(11)]
 
 
 def update_world():
+    grass.update()
+    for boy in team:
+        boy.update()
     pass
 
 
 def render_world():
     clear_canvas()
+    grass.draw()
+    for boy in team:
+        boy.draw()
     update_canvas()
 
 
